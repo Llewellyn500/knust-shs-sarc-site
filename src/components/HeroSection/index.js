41b04a03e9b7128ae9037d025856d7ef1d8ce12f
Llewellyn500 <Llewellynpaintsil34@gmail.com>
2021-03-19 21:26:42 +0000

    🔰Done with HeroSection

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "../../videos/video.m4v";
-import { HeroContainer, HeroBg, VideoBg } from "./HeroElements";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>KNUST SHS Science & Robotics Club</HeroH1>
+        <HeroP>
+          In our country Ghana, innovation starts from SHS, thats why we at
+          KNUST SHS have made it our mission to ignite the zeal for tech in the
+          young ones to come, through science and technology.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="about"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Who Are We? {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
