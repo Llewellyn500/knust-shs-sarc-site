6bbf18789311f443a373bc6a8390c607ff3e1d15
Llewellyn500 <Llewellynpaintsil34@gmail.com>
2021-03-19 17:38:00 +0000

    🚧working on navigations

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {FaBars} from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">SARC</NavLogo>
+          <MobileIcon>
+              <FaBars />
+          </MobileIcon>
+          <NavMenu>
+              <NavItem>
+                  <NavLinks to="about">About</NavLinks>
+              </NavItem>
+              <NavItem>
+                  <NavLinks to="discover">Discover</NavLinks>
+              </NavItem>
+              <NavItem>
+                  <NavLinks to="teams">Teams</NavLinks>
+              </NavItem>
+              <NavItem>
+                  <NavLinks to="signup">Sign Up</NavLinks>
+              </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
